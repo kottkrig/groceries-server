46603b647e4b81f3268a20e7baf6506b06e8bbee
akire <erika@akire.se>
2011-12-19 14:46:12 +0000

    Refactoring: Added spaces to make code more readable.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
+    
     route(handle, pathname, response);
   }
-
+  
   http.createServer(onRequest).listen(process.env.PORT);
   console.log("Server has started.");
 }
