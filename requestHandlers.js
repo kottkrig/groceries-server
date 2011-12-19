11637ffb4fadbc0f2e731e186f682cf851b5a270
Johan Larsson <johan@johanl.se>
2011-12-19 20:27:18 +0000

    Refactored app.js into server.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,18 @@
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var fs = require('fs');
 
 function start(response) {
-  console.log("Request handler 'start' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Start");
-  response.end();
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      response.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(data);
+    response.end();
+  });
 }
 
 function add(response) {
--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 var http = require("http");
+var socketIo = require('./node_modules/socket.io/lib/socket.io'); 
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
     
-    route(handle, pathname, response);
-  }
-  
-  http.createServer(onRequest).listen(process.env.PORT);
-  console.log("Server has started.");
+        route(handle, pathname, response);
+    }
+    
+    var app = http.createServer(onRequest);
+    app.listen(process.env.PORT, '0.0.0.0');
+    console.log("Server has started.");
+    
+    var io = socketIo.listen(app);
+    
+    io.configure(function() {
+        io.set('transports', ['xhr-polling']);
+        io.set('polling duration', 10);
+    });
+    
+    function onConnection(socket) {
+        io.sockets.emit('this', { will: 'be received by everyone'});
+        console.log('Socket connected');
+        socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
+        socket.on('disconnect', function() {
+            console.log('Client disconnected');
+        }); 
+    }
+        
+    io.sockets.on('connection', onConnection);
 }
+
 
 exports.start = start;
