43f93f481b857660dac0dfcc61402db1572cd74f
akire <erika@akire.se>
2011-12-19 14:45:28 +0000

    Implemented sockets. Created client for testing on index.html.

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = require('http').createServer(handler), 
+io = require('./node_modules/socket.io/lib/socket.io').listen(app), 
+fs = require('fs');
+
+app.listen(process.env.PORT, '0.0.0.0');
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {  
+  io.sockets.emit('this', { will: 'be received by everyone'});
+  console.log('Socket connected');
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  socket.on('disconnect', function() {
+    console.log('Client disconnected');
+  }); 
+});
