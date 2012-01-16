eea6fd0b36b33238861e1704e9f8d52d96110dc2
akire <erika@akire.se>
2012-01-16 19:48:25 +0000

    Added redis support. Fixed it so it works like a charm again

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,17 @@
 //var exec = require("child_process").exec;
 var fs = require('fs');
+var redis = require('redis-client');
+var db = redis.createClient(9443, 'stingfish.redistogo.com');
+var dbAuth = function() {
+  db.auth('eeabd0f6182a690e0c0c1df7556c85ae');
+};
 
-function start(response) {
+db.addListener('connected', dbAuth);
+db.addListener('reconnected', dbAuth);
+
+//var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+
+function start(response, postData) {
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
     if (err) {
@@ -15,11 +25,16 @@ function start(response) {
   });
 }
 
-function add(response) {
-  console.log("Request handler 'add' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Add!");
-  response.end();
+function add(response, postData) {   
+    console.log("Request handler 'add' was called.");
+    db.set("message", postData);
+    db.get("message", function(err, value) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+          response.write("Hello Add!");
+          response.write("From database: " + value);
+          response.end();
+    });
+  
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -4,10 +4,19 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = '';
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-    
-        route(handle, pathname, response);
+        
+        request.setEncoding('utf8');
+        
+        request.addListener('data', function(data) {
+            postData += data;
+        });
+        
+        request.addListener('end', function() {
+            route(handle, pathname, response, postData);
+        });
     }
     
     var app = http.createServer(onRequest);
