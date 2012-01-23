f761d70fbe3b31578a7ad05e4bf3774fc6d1b513
Erika Thorsen <erika@akire.se>
2012-01-23 12:28:01 +0000

    Cleaned up code, removed unused socket code

--- router.js
+++ router.js
@@ -1,14 +1,9 @@
-
-function route(handle, pathname, response, data, io) {
+function route(handle, pathname, response, data) {
   console.log("About to route a request for " + pathname);
   
   if (typeof handle[pathname] === 'function') {
-      handle[pathname](response, data, io);
-  } else {
-      
-      
-      
-      
+      handle[pathname](response, data);
+  } else {  
     console.log("No request handler found for " + pathname);
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.write("404 Not found");
