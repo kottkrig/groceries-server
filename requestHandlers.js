91292520cea886d263c25fca4557b01251564ab4
Erika Thorsen <erika@akire.se>
2012-04-18 09:37:24 +0000

    Removed unnecessary calls to DB to fetch list names

--- requestHandlers.js
+++ requestHandlers.js
@@ -8,8 +8,6 @@ db.addListener('connected', dbAuth);
 db.addListener('reconnected', dbAuth);
 dbAuth();
 
-var ACTIVE_ID = 'activeId';
-var DONE_ID = 'doneId';
 var EMPTY_SET = 'empty';
 var LAST_LIST_ID = 0;
 
@@ -20,74 +18,59 @@ function newList(callback) {
     db.incr(LAST_LIST_ID, function(err, newListId) {
         if(err)
             return callback('Could not create new list');
-        db.hset(newListId, ACTIVE_ID, newListId + '_active');
-        db.hset(newListId, DONE_ID, newListId + '_done');     
         LAST_LIST_ID = newListId;
         return callback(null, newListId);
     });
 }    
 
 function add(listId, item, callback) {
-    console.log('Item: ' + item);
-    db.hget(listId, ACTIVE_ID, function(err, activeListId) {
+    db.zcard(activeList(listId), function(err, cardinality) {
         if(err)
-            return callback('Could not find list');
-        db.zcard(activeListId, function(err, cardinality) {
+            return callback('Internal server error');
+        db.zadd(activeList(listId), cardinality, item, function(err) {
             if(err)
-                return callback('Internal server error');
-            db.zadd(activeListId, cardinality, item, function(err) {
-                if(err)
-                    return callback('Could not add item');
-                return callback(null, listId, item);
-            });
+                return callback('Could not add item');
+            return callback(null, listId, item);
         });
     });
 }
 
 function remove(listId, item, callback) {
-    db.hget(listId, ACTIVE_ID, function(err, activeListId) {
+    db.zrem(activeList(listId), item, function(err, value) {
         if(err)
-            return callback('Could not find list');
-        db.hget(listId, DONE_ID, function(err, doneListId) {
+            return callback('Could not remove item');
+        if(value != 1)
+            return callback(null, listId, item); 
+        db.zadd(doneList(listId), 0, item, function(err) {
             if(err)
-                return callback('Could not find list');
-            db.zrem(activeListId, item, function(err, value) {
-                if(err)
-                    return callback('Could not remove item');
-                if(value != 1)
-                    return callback(null, listId, item); 
-                db.zadd(doneListId, 0, item, function(err) {
-                    if(err)
-                        return callback('Could not add item to DONE');    
-                    return callback(null, listId, item);
-                }); 
-            });
-        });
+                return callback('Could not add item to DONE');    
+            return callback(null, listId, item);
+        }); 
     });
 }
 
 function getList(listId, callback) {    
-    db.hget(listId, ACTIVE_ID, function(err, activeListId) {
+    db.zrange(activeList(listId), FIRST_ITEM, LAST_ITEM, function(err, items) {
         if(err)
-            return callback('Could not find list');   
-        db.zrange(activeListId, FIRST_ITEM, LAST_ITEM, function(err, items) {
-            if(err)
-                return callback('Error when fetching list from database');   
-            return callback(null, items);
-        });  
-    });
+            return callback('Error when fetching list from database');   
+        return callback(null, items);
+    });  
 }
 
 function clearList(listId, callback) {
-    db.hget(listId, ACTIVE_ID, function(err, activeListId) {
+    db.zinterstore(activeList(listId), 2, activeList(listId), EMPTY_SET, function(err) {
         if(err)
-            return callback('Could not find list');
-        db.zinterstore(activeListId, 2, activeListId, EMPTY_SET, function(err) {
-            if(err)
-                return callback('Could not clear list');    
-            return callback(null);
-        });
+            return callback('Could not clear list');    
+        return callback(null);
     });
+}
+
+function activeList(listId) {
+    return listId + '_active';
+}
+
+function doneList(listId) {
+    return listId + '_done';
 }
 
 exports.add = add;
